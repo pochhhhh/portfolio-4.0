0b7b8063621406bd93d94c91296de6210baade33
Marlon Pempengco <Marlon@24-7languageservices.local>
2018-10-17 17:27:00 +0100

    Added CSS animations for homepage and contact page

---
+++ scripts.js
@@ -0,0 +1,16 @@
+/*
+$(document).ready(function(){
+
+
+  $('main .header-showcase .section-a h1').animate({left: '0'}, 1000);
+
+  $('main .header-showcase .section-a').click(function(){
+
+    $('main .header-showcase .section-a h1').animate({left: '1000px'}, 1000);
+
+  })
+
+
+});
+
+*/

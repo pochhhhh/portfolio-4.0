b36a6ff3f6c8e1516bd15b564c92d291ea709f68
Marlon <marlonpempengco@gmail.com>
2019-03-10 12:43:27 +0000

    added mobile responsiveness

--- scripts.js
+++ scripts.js
@@ -1,16 +1,41 @@
-/*
-$(document).ready(function(){
+class Typewriter{
 
 
-  $('main .header-showcase .section-a h1').animate({left: '0'}, 1000);
+  constructor(){
 
-  $('main .header-showcase .section-a').click(function(){
-
-    $('main .header-showcase .section-a h1').animate({left: '1000px'}, 1000);
-
-  })
+    this.showCaseText = document.querySelector('.header-showcase .section-a .caption h1');
+    this.captionlastSentence = this.showCaseText.textContent.substring(16, this.showCaseText.textContent.length);
+    this.events();
 
 
-});
+  }
 
-*/
+
+  events(){
+
+
+
+
+
+
+  }
+
+  //Methods
+
+  run(){
+
+    if(this.captionlastSentence === "I build websites_"){
+
+      console.log(true);
+
+    }
+
+
+  }
+
+}
+
+var typewriter = new Typewriter();
+
+
+typewriter.run();
